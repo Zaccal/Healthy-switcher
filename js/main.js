4706866a4f5e949e7c26bcc5766d874b5174a718
Adlet <ksss90411@gmail.com>
2025-11-04 16:56:29 +0500

    [fix]: fixed problems

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
-< !--Yandex.Metrika counter-- >
-<script type="text/javascript">
-    (function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105110290', 'ym');
-
-    ym(105110290, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/105110290" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-
 // Burger 
 const burgerBtn = document.querySelector('.burger-btn')
 const burgerMenu = document.querySelector('.burger-menu')
